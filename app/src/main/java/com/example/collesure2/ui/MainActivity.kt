137779b52663d90bc1fbe23df3099156a2af6449
Daishin16 <sakuya1202@icloud.com>
2019-10-31 12:24:08 +0900

    first commit

---
+++ app/src/main/java/com/example/collesure2/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.collesure2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.collesure2.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
