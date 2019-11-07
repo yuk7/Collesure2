3ce8a03a35058e0ff3991b3ab6e058ce3e50eb1d
yuk7 <yukx00@gmail.com>
2019-11-07 13:37:43 +0900

    layout/activity_main: use FrameLayout to fragment

--- app/src/main/java/com/example/collesure2/ui/MainActivity.kt
+++ app/src/main/java/com/example/collesure2/ui/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.collesure2.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.collesure2.R
+import com.example.collesure2.ui.user.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment, SearchFragment()).commit()
     }
 
 }
