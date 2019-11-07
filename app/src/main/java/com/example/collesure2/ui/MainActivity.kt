2c132dd12826d666b2e748259ef2feb5f501ea06
yuk7 <yukx00@gmail.com>
2019-11-07 19:07:40 +0900

    ui: Implement bottom navigation controller

--- app/src/main/java/com/example/collesure2/ui/MainActivity.kt
+++ app/src/main/java/com/example/collesure2/ui/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.collesure2.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.collesure2.R
+import com.example.collesure2.ui.bnv.Controller
 import com.example.collesure2.ui.user.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation_view.setOnNavigationItemSelectedListener(Controller(supportFragmentManager).navigationListener)
         supportFragmentManager.beginTransaction().replace(R.id.fragment, SearchFragment()).commit()
     }
 
--- app/src/main/java/com/example/collesure2/ui/bnv/Controller.kt
+++ app/src/main/java/com/example/collesure2/ui/bnv/Controller.kt
@@ -1,5 +1,26 @@
 package com.example.collesure2.ui.bnv
 
-class Controller { //bottom navigation view用
+import androidx.fragment.app.FragmentManager
+import com.example.collesure2.R
+import com.example.collesure2.ui.user.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
+class Controller (val fm:FragmentManager){ //bottom naviation view用
+
+    val navigationListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        //clear BackStack
+        for (i in 0 until fm.backStackEntryCount) {
+            fm.popBackStack()
+        }
+
+        when (item.itemId) {
+            R.id.home -> {
+                fm.beginTransaction()
+                    .replace(R.id.fragment, SearchFragment())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 }
