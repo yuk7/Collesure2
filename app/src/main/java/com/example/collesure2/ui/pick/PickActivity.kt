9c88a6209e6b48b7aee9ee2b2d2f4283bfe4bf9d
yuk7 <yukx00@gmail.com>
2019-12-06 02:45:06 +0900

    ui: PickActivity: add loading and error fallback

--- app/src/main/java/com/example/collesure2/ui/pick/PickActivity.kt
+++ app/src/main/java/com/example/collesure2/ui/pick/PickActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.example.collesure2.R
 import com.example.collesure2.data.ImageItem
@@ -15,7 +16,18 @@ class PickActivity : AppCompatActivity() {
         setContentView(R.layout.activity_image)
         val imageView = findViewById<ImageView>(R.id.pickUpImage)
         val item = intent.getSerializableExtra("item") as ImageItem
-        Glide.with(this).load(item.imageUrl).into(imageView)
+
+        val circular = CircularProgressDrawable(this)
+        circular.strokeWidth = 8f
+        circular.centerRadius = 80f
+        circular.start()
+
+        Glide.with(this)
+            .load(item.imageUrl)
+            .placeholder(circular)
+            .error(Glide.with(this).load(item.thumbIUrl)
+                .error(R.drawable.ic_error_red_24dp))
+            .into(imageView)
     }
 
 }
