61b170d3aed3a3fe9850601955b231fa5106dd21
Daishin16 <sakuya1202@icloud.com>
2019-12-05 15:37:54 +0900

    init pick

---
+++ app/src/main/java/com/example/collesure2/ui/pick/PickActivity.kt
@@ -0,0 +1,13 @@
+package com.example.collesure2.ui.pick
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PickActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+}
