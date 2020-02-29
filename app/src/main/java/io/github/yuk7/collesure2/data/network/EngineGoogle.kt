2b1ce338b3eb8925e7b5b7698fd168d5b2493e7e
yuk7 <yukx00@gmail.com>
2020-02-29 18:45:42 +0900

    data: network: EngineGoogle: Support for new specification

--- app/src/main/java/io/github/yuk7/collesure2/data/network/EngineGoogle.kt
+++ app/src/main/java/io/github/yuk7/collesure2/data/network/EngineGoogle.kt
@@ -1,10 +1,14 @@
 package io.github.yuk7.collesure2.data.network
 
+import android.util.Log
+import androidx.core.util.rangeTo
+import com.google.gson.Gson
 import io.github.yuk7.collesure2.data.ImageItem
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.util.regex.Pattern
 import okhttp3.HttpUrl
+import org.json.JSONArray
 
 
 class EngineGoogle : SearchEngine {
@@ -28,63 +32,41 @@ class EngineGoogle : SearchEngine {
     }
 
     private fun searchAllImage(word: String, nsfw: Boolean): ArrayList<ImageItem> {
-        var jsons = parseHtml(getHtml(word, nsfw))
-        var items = arrayListOf<ImageItem>()
-        for (json in jsons) {
-            var item = parseJson(json)
-            item.tag = word
-            items.add(item)
+        var ds2 = parseHtml(getHtml(word, nsfw))
+        val json_items = parseJson(ds2)
+        val items = arrayListOf<ImageItem>()
+        for(i in 0 until json_items.length()-1) {
+            val json_item = json_items.getJSONArray(i)
+            if(json_item.getInt(0) == 1) {
+                val json_item_img = json_item.getJSONArray(1)
+                val item = ImageItem()
+                item.imageUrl = json_item_img.getJSONArray(3).getString(0)
+                item.thumbIUrl = json_item_img.getJSONArray(2).getString(0)
+                item.url = json_item_img.getJSONObject(9).getJSONArray("2003").getString(2)
+                items.add(item)
+            }
         }
         return items
     }
 
-    private fun parseJson(json_txt: String): ImageItem {
-        val regex_orig = "\"ou\":\"(.+?)\""
-        val regex_thumb = "\"tu\":\"(.+?)\""
-        val regex_url = "\"ru\":\"(.+?)\""
-        val matcher_orig = Pattern.compile(regex_orig).matcher(json_txt)
-        val matcher_thumb = Pattern.compile(regex_thumb).matcher(json_txt)
-        val matcher_url = Pattern.compile(regex_url).matcher(json_txt)
-
-        var item = ImageItem()
-        if (matcher_orig.find()) {
-            item.imageUrl = urlDeEscape(matcher_orig.group(1)!!)
-        }
-        if (matcher_thumb.find()) {
-            var thumbUrl = urlDeEscape(matcher_thumb.group(1)!!)
-            item.thumbIUrl = thumbUrl
-        }
-        if (matcher_url.find()) {
-            item.url = urlDeEscape(matcher_url.group(1)!!)
-        }
-        return item
+    private fun parseJson(json: String): JSONArray {
+        val root = JSONArray(json)
+        val r31_0_12_2 = root.getJSONArray(31).getJSONArray(0).getJSONArray(12).getJSONArray(2)
+        return r31_0_12_2
     }
 
-    private fun urlDeEscape(urlStr: String): String {
-        var result = urlStr
-        result = result.replace("\\u003d", "=")
-        result = result.replace("\\u0026", "&")
-        result = result.replace("\\u003c", "<")
-        result = result.replace("\\u003e", ">")
-
-        return result
-    }
-
-    private fun parseHtml(html_txt: String): ArrayList<String> {
-        val regex = "<div class=\"rg_meta notranslate\">(.+?)</div>"
-        val pattern = Pattern.compile(regex)
+    private fun parseHtml(html_txt: String): String {
+        val regex = "AF_initDataCallback\\(\\{key: 'ds:2', isError:  false , hash: '3', data:function\\(\\)\\{return ((?m).+?)\\}\\}\\);</script>"
+        val pattern = Pattern.compile(regex, Pattern.DOTALL)
         val matcher = pattern.matcher(html_txt)
-        val imageJsonList = arrayListOf<String>()
-        while (matcher.find()) {
-            imageJsonList.add(matcher.group(1)!!)
-        }
-        return imageJsonList
+        matcher.find()
+        return matcher.group(1)!!
     }
 
     private fun getHtml(word: String, nsfw: Boolean): String {
         val client = OkHttpClient()
         var urlbuilder = HttpUrl.Builder()
-            .scheme("http")
+            .scheme("https")
             .host("www.google.com")
             .addPathSegment("search")
             .addQueryParameter("q", word)
@@ -98,7 +80,7 @@ class EngineGoogle : SearchEngine {
         val req = Request.Builder().url(urlbuilder.build())
             .header(
                 "User-Agent",
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/82.0.4068.5 Safari/537.36"
             ).get().build()
         val resp = client.newCall(req).execute()
         return resp.body!!.string()
