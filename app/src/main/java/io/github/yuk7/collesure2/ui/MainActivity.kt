71015eed6d206b03bf50206f6210fdeb1a7b7943
yuk7 <yukx00@gmail.com>
2020-03-06 03:02:30 +0900

    Preference: Implement Theme Selector

--- app/src/main/java/io/github/yuk7/collesure2/ui/MainActivity.kt
+++ app/src/main/java/io/github/yuk7/collesure2/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package io.github.yuk7.collesure2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
+import androidx.preference.PreferenceManager
 import io.github.yuk7.collesure2.R
 import io.github.yuk7.collesure2.ui.bnv.Controller
 import io.github.yuk7.collesure2.ui.user.SearchFragment
@@ -14,6 +17,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         bottom_navigation_view.setOnNavigationItemSelectedListener(Controller(supportFragmentManager).navigationListener)
         supportFragmentManager.beginTransaction().replace(R.id.fragment, SearchFragment()).commit()
+
+        val prefs =  PreferenceManager.getDefaultSharedPreferences(baseContext)
+        when (prefs.getString("select_theme","0")) {
+            "0" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_UNSPECIFIED)
+            }
+            "1" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            "2" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+
     }
 
 }
